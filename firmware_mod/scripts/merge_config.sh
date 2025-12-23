31571faccf6d99754435ebb80ee10eac9ec861ea
MDW <mdeweerd@users.noreply.github.com>
2025-12-23 20:10:21 +0100

    :wrench: Update shellcheck configuration and merge script
    - Added error code 2143 to shellcheck args to ignore specific warnings
    - Fixed variable quoting in merge_config.sh for better shell compatibility
    - Improved comparison operator in merge_config.sh

--- firmware_mod/scripts/merge_config.sh
+++ firmware_mod/scripts/merge_config.sh
@@ -12,13 +12,13 @@ for CONF in $CONF_LIST; do
         CONF_DIST_VARS=$(grep -o '^[^#]*' "${CONF_PATH}/${CONF}.dist" | cut -d= -f1 | sort)
         CONF_USER_VARS=$(grep -o '^[^#]*' "${CONF_PATH}/${CONF}" | cut -d= -f1 | sort)
         for VAR in $CONF_DIST_VARS; do
-            if [ ! -n  "$(echo $CONF_USER_VARS | grep $VAR)" ]; then
+            if [ ! -n  "$(echo "$CONF_USER_VARS" | grep "$VAR")" ]; then
                 CONF_NEW_VARS_COUNT=$((CONF_NEW_VARS_COUNT+1))
                 echo "# Parameter added by merge tool" >> "${CONF_PATH}/${CONF}"
                 grep "$VAR" "${CONF_PATH}/${CONF}.dist" >> "${CONF_PATH}/${CONF}"
             fi
         done
-        if [ $CONF_NEW_VARS_COUNT == 0 ]; then
+        if [ "$CONF_NEW_VARS_COUNT" = 0 ]; then
             echo "No new parameters between ${CONF} ${CONF}.dist"
         else
             echo "Merging ${CONF_NEW_VARS_COUNT} parameters in ${CONF}"
